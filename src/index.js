d07a77a1d651476c0e1b84ec8fc60af78f7264bd
shivam tripathi <2002shiv01@gmail.com>
2023-03-28 23:04:36 +0530

    here i am adding <h1></h1> tag

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+var React = require('react');
+var ReactDOM = require('react-dom');
+// A fragment looks like an empty HTML tag: <></>.
+
+const myElement = ( <>
+<h1>shivam hello</h1>
+</>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
